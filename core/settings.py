33f2ed122a66076a519047b3712a3cfa7d9c6a81
akashSharma135 <64312383+akashSharma135@users.noreply.github.com>
2021-12-16 11:33:54 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -127,8 +127,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "flaskymailtest@gmail.com"
-EMAIL_HOST_PASSWORD = "Akash135#"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 
 
 RAZOR_KEY_ID = 'rzp_test_zDw4mkdJiaEL7X'
